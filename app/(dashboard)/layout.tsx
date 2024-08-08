46e180df97c09e41aeeaf8dfe60f0740a7054e5e
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-08-08 15:13:49 +0200

    Layout is getting poppine.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,10 +4,18 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen flex-col-reverse md:flex-row">
+    <div className="flex min-h-screen flex-col-reverse md:flex-row">
       {/* from-blue-500 from-[#5882f2] to-cyan-500 to-[#0fb8cb] */}
-      <div className="h-[102px] w-auto bg-gradient-to-r from-[#5882f2] to-[#0fb8cb] md:h-auto md:w-[102px] md:bg-gradient-to-b"></div>
+      <div>
+        <div className="fixed inset-x-0 bottom-0 top-auto z-50 h-[102px] w-auto shrink-0 bg-gradient-to-r from-[#5882f2] to-[#0fb8cb] md:inset-y-0 md:left-0 md:right-auto md:h-auto md:w-[102px] md:bg-gradient-to-b"></div>
+        <div className="h-[102px] w-auto shrink-0 bg-transparent md:h-auto md:w-[102px]"></div>
+      </div>
       <div className="h-full">{children}</div>
     </div>
+    // <>{children}</>
   );
 }
+
+/* Notes
+Let's try something for the overscroll.
+*/
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,11 @@ export default function RootLayout({
   return (
     // <html lang="en">
     <html lang="fr">
-      <body>{children}</body>
+      <body className="bg-teal-50">{children}</body>
+      {/* <body className="flex min-h-screen flex-col-reverse md:flex-row">
+        <div className="fixed bottom-0 left-0 top-0 h-[102px] w-auto shrink-0 bg-gradient-to-r from-[#5882f2] to-[#0fb8cb] md:h-auto md:w-[102px] md:bg-gradient-to-b"></div>
+        <div className="h-full">{children}</div>
+      </body> */}
     </html>
   );
 }
