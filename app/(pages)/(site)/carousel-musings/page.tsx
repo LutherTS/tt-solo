93c6057d5991c66ddcda4834093472c6682b3f8a
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-11-04 17:13:11 +0100

    Server- (and client-)side carousel musings.

---
+++ app/(pages)/(site)/carousel-musings/page.tsx
@@ -0,0 +1,91 @@
+// app/page.tsx or app/carousel/page.tsx
+
+// import { redirect } from "next/navigation";
+import { LeftClientButton, RightClientButton } from "./client-buttons";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { slide?: string };
+}) {
+  searchParams = await searchParams;
+
+  const items = ["Item 1", "Item 2", "Item 3"];
+  const currentIndex = parseInt(searchParams.slide || "0", 10) || 0;
+
+  // async function nextIndex(formData: FormData) {
+  //   "use server";
+
+  //   const currentIndex = Number(formData.get("currentIndex") || 0);
+  //   const next = currentIndex + 1;
+  //   redirect("/carousel?slide=" + next);
+  // }
+
+  // async function previousIndex(formData: FormData) {
+  //   "use server";
+
+  //   const currentIndex = Number(formData.get("currentIndex") || 0);
+  //   const prev = currentIndex - 1;
+  //   redirect("/carousel?slide=" + prev);
+  // }
+
+  return (
+    <div>
+      <h1>Carousel Example</h1>
+      <Carousel
+        items={items}
+        currentIndex={currentIndex}
+        // nextIndex={nextIndex}
+        // previousIndex={previousIndex}
+      />
+    </div>
+  );
+}
+
+const Carousel = ({
+  items,
+  currentIndex,
+  // nextIndex,
+  // previousIndex,
+}: {
+  items: string[];
+  currentIndex: number;
+  // nextIndex: any;
+  // previousIndex: any;
+}) => {
+  return (
+    <>
+      <div className="carousel">
+        <div
+          className="carousel-container"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {items.map((item, index) => (
+            <div className="carousel-item" key={index}>
+              {item}
+            </div>
+          ))}
+        </div>
+        {/* <form action={previousIndex}>
+          <input type="hidden" name="currentIndex" value={currentIndex} />
+          <button type="submit" className="carousel-button prev">
+            Previous
+          </button>
+        </form> */}
+        <LeftClientButton currentIndex={currentIndex} />
+        {/* <form action={nextIndex}>
+          <input type="hidden" name="currentIndex" value={currentIndex} />
+          <button type="submit" className="carousel-button next">
+            Next
+          </button>
+        </form> */}
+        <RightClientButton currentIndex={currentIndex} />
+      </div>
+    </>
+  );
+};
+
+/* Notes
+Switch pages with redirect provokes a full refresh. (Which is weird because I don't remember it being like this on my moments page?) So that's why a client component as usual would be required for navigation.
+Exactly. So always prefer useRouter to get navigation client-side without hard refreshes.
+*/
