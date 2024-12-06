f85076c1f541f609028a789bf0414eec2abfb7c8
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-12-06 20:17:28 +0100

    /moments-bis as default.

--- app/constants/agnostic/globals.ts
+++ app/constants/agnostic/globals.ts
@@ -45,7 +45,7 @@ export const navLinks: NavLink[] = [
   {
     id: "Moments",
     label: "Moments",
-    href: "/moments",
+    href: "/moments-bis",
     icon: "CalendarDaysOutlineIcon",
   },
   {
