4b74c2b481fc55b33ab1f9ee7ca0ca283fec6857
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-12-12 16:31:12 +0100

    All demos made.

---
+++ app/(pages)/(dashboard)/users/[username]/moments-bis-3/components/pre-agnostic/index.ts
@@ -0,0 +1,20 @@
+/* IMPORTS */
+
+// External imports
+
+// import * as fs from "node:fs"; // to prove this is not a Server Module
+// fs.readdirSync("/"); // this is a NOT imported as a Server Module, so it will throw an error
+
+// Internal imports
+
+import { LocalAgnosticComponentsName } from "./local";
+import { DateCardsAgnosticComponentsName } from "./date-cards";
+
+/* LOGIC */
+
+export * from "./local";
+export * from "./date-cards";
+
+export type AllLocalAgnosticComponentsName =
+  | LocalAgnosticComponentsName
+  | DateCardsAgnosticComponentsName;
