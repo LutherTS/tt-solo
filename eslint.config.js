2aee6ec9ede1c74a4aa689083e49ccebd0700f17
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-04-14 12:03:12 +0200

    defineConfig instead of tseslint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,28 @@
-// @ts-check
+import { defineConfig } from "eslint/config";
 
-import eslint from "@eslint/js";
+// import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import globals from "globals";
 
-export default tseslint.config(eslint.configs.recommended);
+export default defineConfig([
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        React: "readonly",
+      },
+    },
+    rules: {
+      // ...eslint.configs.recommended.rules,
+      // "func-style": "warn",
+    },
+  },
+]);
