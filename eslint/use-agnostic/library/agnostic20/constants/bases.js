bc84049d0cf9b1b0fe39cb0ac471e2af0f15736f
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-04-24 16:10:16 +0200

    OK, commentedDirectives_BlockedImports now.

--- eslint/use-agnostic/library/agnostic20/constants/bases.js
+++ eslint/use-agnostic/library/agnostic20/constants/bases.js
@@ -88,11 +88,9 @@ const makeIntroForSpecificViolationMessage = (
 
 export const effectiveDirectives_BlockedImports = Object.freeze({
   [USE_SERVER_LOGICS]: [
-    {
-      // problematic (because if server function can now import each other, than it's only logical that there would be server logic manipulate one with the other)
-      blockedImport: USE_SERVER_FUNCTIONS,
-      message: `${makeIntroForSpecificViolationMessage(USE_SERVER_LOGICS, USE_SERVER_FUNCTIONS)} Server Functions are only to be triggered by Client Components.`,
-    },
+    // USE_SERVER_LOGICS allowed, because Server Logics can compose between one another.
+    // USE_SERVER_COMPONENTS allowed, because Server Components are OK to be composed with Server Logics as long as the Server Logics Module, by convention, does not export React components.
+    // USE_SERVER_FUNCTIONS allowed, because as Server Functions can import one another, it is only natural that they could compose through Server Logics.
     {
       blockedImport: USE_CLIENT_LOGICS,
       message: `${makeIntroForSpecificViolationMessage(USE_SERVER_LOGICS, USE_CLIENT_LOGICS)} Client logic should never leak to the server.`,
@@ -101,28 +99,28 @@ export const effectiveDirectives_BlockedImports = Object.freeze({
       blockedImport: USE_CLIENT_COMPONENTS,
       message: `${makeIntroForSpecificViolationMessage(USE_SERVER_LOGICS, USE_CLIENT_COMPONENTS)} Client Components cannot be tinkered with on the server.`,
     },
+    // USE_AGNOSTIC_LOGICS allowed, because Agnostic Logic can run safely on the server just like it can on the client
+    // USE_AGNOSTIC_COMPONENTS allowed, because Agnostic Components can be composed with Logics on the server just like they can on the client, again, as long at the Server Logics Module, by convention, does not export React components.
   ],
   [USE_SERVER_COMPONENTS]: [
-    // problematic
-    {
-      blockedImport: USE_SERVER_FUNCTIONS,
-      message: `${makeIntroForSpecificViolationMessage(USE_SERVER_COMPONENTS, USE_SERVER_FUNCTIONS)} Server Components Modules can make their own Server Functions through inline 'use server' directives.`,
-    },
+    // USE_SERVER_LOGICS allowed, because logic from the server can safely support Server Components.
+    // USE_SERVER_COMPONENTS allowed, because Server Components can composed with one another, assuming thanks to the inclusion of the 'use agnostic' directive that they are actual Server Components.
+    // USE_SERVER_FUNCTIONS allowed, because as Server Components Modules can import Client Components, they are able to pass them Server Functions as props as well, even though indeed Server Components Modules can make their own Server Functions through inline 'use server' directives.
     {
       blockedImport: USE_CLIENT_LOGICS,
       message: `${makeIntroForSpecificViolationMessage(USE_SERVER_COMPONENTS, USE_CLIENT_LOGICS)} Client logic should never leak to the server.`,
     },
+    // USE_CLIENT_COMPONENTS allowed, because Client Components can be nested inside Server Components either to wrap some of the tree with client state accessible through child Client Components, or to create client boundaries when the root of the application is planted on the server.
+    // USE_AGNOSTIC_LOGICS allowed, because Agnostic Logic can run safely on the server just like it can on the client.
+    // USE_AGNOSTIC_COMPONENTS allowed, because Agnostic Components can render safely on the server just like they can on the client.
   ],
   [USE_SERVER_FUNCTIONS]: [
+    // USE_SERVER_LOGICS allowed, because logic from the server can safely support Server Functions.
     {
       blockedImport: USE_SERVER_COMPONENTS,
       message: `${makeIntroForSpecificViolationMessage(USE_SERVER_FUNCTIONS, USE_SERVER_COMPONENTS)} Server Functions have no business working with React Components.`,
     },
-    {
-      // problematic
-      blockedImport: USE_SERVER_FUNCTIONS,
-      message: `${makeIntroForSpecificViolationMessage(USE_SERVER_FUNCTIONS, USE_SERVER_FUNCTIONS)} Server Functions don't need to import one another. Import their logic through Server Logics Modules instead. (Made with no directive and no JSX extension.)`,
-    },
+    // USE_SERVER_FUNCTIONS allowed, because even though Server Functions don't need to import one another and the same results can be generated via Server Logic alone for the outcome of a single Server Function, a rational use case could be found for composing Server Functions with one another either today or in the future.
     {
       blockedImport: USE_CLIENT_LOGICS,
       message: `${makeIntroForSpecificViolationMessage(USE_SERVER_FUNCTIONS, USE_CLIENT_LOGICS)} Client logic should never leak to the server.`,
@@ -131,6 +129,7 @@ export const effectiveDirectives_BlockedImports = Object.freeze({
       blockedImport: USE_CLIENT_COMPONENTS,
       message: `${makeIntroForSpecificViolationMessage(USE_SERVER_FUNCTIONS, USE_CLIENT_COMPONENTS)} Server Functions have no business working with React Components.`,
     },
+    // USE_AGNOSTIC_LOGICS allowed, because Agnostic Logic can run safely on the server just like it can on the client
     {
       blockedImport: USE_AGNOSTIC_COMPONENTS,
       message: `${makeIntroForSpecificViolationMessage(USE_SERVER_FUNCTIONS, USE_AGNOSTIC_COMPONENTS)} Server Functions have no business working with React Components.`,
@@ -149,6 +148,10 @@ export const effectiveDirectives_BlockedImports = Object.freeze({
       blockedImport: USE_SERVER_FUNCTIONS,
       message: `${makeIntroForSpecificViolationMessage(USE_CLIENT_LOGICS, USE_SERVER_FUNCTIONS)} Server Functions only interact with the client through Client Components.`,
     },
+    // USE_CLIENT_LOGICS allowed, because Client Logics can compose between one another.
+    // USE_CLIENT_COMPONENTS allowed, because Client Components are OK to be composed with Client Logics as long as the Client Logics Module, by convention, does not export React components.
+    // USE_AGNOSTIC_LOGICS allowed, because Agnostic Logic can run safely on the client just like it can on the server
+    // USE_AGNOSTIC_COMPONENTS allowed, because Agnostic Components can be composed with Logics on the client just like they can on the server, again, as long at the Client Logics Module, by convention, does not export React components.
   ],
   [USE_CLIENT_COMPONENTS]: [
     {
@@ -159,6 +162,11 @@ export const effectiveDirectives_BlockedImports = Object.freeze({
       blockedImport: USE_SERVER_COMPONENTS,
       message: `${makeIntroForSpecificViolationMessage(USE_CLIENT_COMPONENTS, USE_SERVER_COMPONENTS)} Server Components may only pass through Client Components through the children prop within Server Components Modules.`,
     },
+    // USE_SERVER_FUNCTIONS allowed, because Server Functions are specifically triggered by Client Components.
+    // USE_CLIENT_LOGICS allowed, because logic from the client can safely support Client Components.
+    // USE_CLIENT_COMPONENTS allowed, because Client Components can composed with one another.
+    // USE_AGNOSTIC_LOGICS allowed, because Agnostic Logic can run safely on the client just like it can on the server.
+    // USE_AGNOSTIC_COMPONENTS allowed, because Agnostic Components can render safely on the client just like they can on the server.
   ],
   [USE_AGNOSTIC_LOGICS]: [
     {
@@ -181,6 +189,8 @@ export const effectiveDirectives_BlockedImports = Object.freeze({
       blockedImport: USE_CLIENT_COMPONENTS,
       message: `${makeIntroForSpecificViolationMessage(USE_AGNOSTIC_LOGICS, USE_CLIENT_COMPONENTS)} Client Components cannot be tinkered with on both the server and the client.`,
     },
+    // USE_AGNOSTIC_LOGICS allowed, because Agnostic Logics can compose between one another.
+    // USE_AGNOSTIC_COMPONENTS allowed, because Agnostic Components can be composed with Logics agnostically as long as at the Agnositc Logics Module, by convention, does not export React components.
   ],
   [USE_AGNOSTIC_COMPONENTS]: [
     {
@@ -191,14 +201,13 @@ export const effectiveDirectives_BlockedImports = Object.freeze({
       blockedImport: USE_SERVER_COMPONENTS,
       message: `${makeIntroForSpecificViolationMessage(USE_AGNOSTIC_COMPONENTS, USE_SERVER_COMPONENTS)} Unlike Client Components, Server Components cannot make a silo of their own once on the client, and can therefore not be executed from the client.`,
     },
-    // problematic
-    {
-      blockedImport: USE_SERVER_FUNCTIONS,
-      message: `${makeIntroForSpecificViolationMessage(USE_AGNOSTIC_COMPONENTS, USE_SERVER_FUNCTIONS)} Though Server Functions could be passed to Client Components in Agnostic Components Modules, prefer importing them directly in Client Components Modules instead.`,
-    },
+    // USE_SERVER_FUNCTIONS allowed, because as Agnostic Components Modules can import Client Components, they are able to pass them Server Functions as props as well.
     {
       blockedImport: USE_CLIENT_LOGICS,
       message: `${makeIntroForSpecificViolationMessage(USE_AGNOSTIC_COMPONENTS, USE_CLIENT_LOGICS)} Client Logic cannot run in both the server and the client.`,
     },
+    // USE_CLIENT_COMPONENTS allowed, because Client Components can be nested inside Agnostic Components either to wrap some of the tree with client state accessible through child Client Components, or to create client boundaries when the root of the application is planted on the server.
+    // USE_AGNOSTIC_LOGICS allowed, because environment-agnostic logic can safely support Agnostic Components.
+    // USE_CLIENT_COMPONENTS allowed, because Client Components can composed with one another.
   ],
 });
